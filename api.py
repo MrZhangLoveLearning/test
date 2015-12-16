fbaab31ef51276c77986fbbd76c9aeb4763b6f7b
mrzhang <2529450174@qq.com>
2015-12-16 20:32:34 +0800

    add allowed_cross_domain

--- api.py
+++ api.py
@@ -1,12 +1,27 @@
 # coding:utf-8
 from flask import Flask, abort, jsonify, request
+from functools import wraps
+from flask import make_response
 import sys
 import main
 app = Flask(__name__)
 app.debug=True
 app.secret_key='api'
 
+def allow_cross_domain(fun):
+    @wraps(fun)
+    def wrapper_fun(*args, **kwargs):
+        rst = make_response(fun(*args, **kwargs))
+        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        allow_headers = "Referer,Accept,Origin,User-Agent"
+        rst.headers['Access-Control-Allow-Headers'] = allow_headers
+        return rst
+    return wrapper_fun
+
+
 @app.route('/api/get/<username>', methods=['GET', 'POST'])
+@allow_cross_domain
 def get_info(username):
     if request.method == 'POST':
         abort(404)
@@ -22,6 +37,7 @@ def get_info(username):
 
 
 @app.route('/api/post', methods=['GET', 'POST'])
+@allow_cross_domain
 def post_info():
     if request.method == 'GET':
         abort(404)
